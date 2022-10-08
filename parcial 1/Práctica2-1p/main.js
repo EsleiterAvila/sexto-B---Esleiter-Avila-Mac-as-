7a0ff4cf380da78884401a5533d8bf8f32b98d0d
esleiteravila199923 <78065244+EsleiterAvila@users.noreply.github.com>
2022-10-07 22:47:17 -0500

    Update main.js

--- parcial 1/Práctica2-1p/main.js
+++ parcial 1/Práctica2-1p/main.js
@@ -1,125 +1,115 @@
 const autos=[
     {
-    
-        autos :1,
+        id :1,
         descripcion:"chevrolet",
         placa: "ur574",
         color: "azul",
-        
-    
-},
+    },
     {
-        autos:2,
+        id:2,
         descripcion:"Mazda",
         placa: "fg344",
         color: "blanco"
     },
     {
-        autos:3,
+        id:3,
         descripcion:"kia",
         placa: "tr546",
         color: "azul"
     },
     {
-        autos:4,
+        id:4,
         descripcion:"Hyundai",
         placa: "gy747",
         color: "amarillo"
     },
     {
-        autos:5,
+        id:5,
         descripcion:"Ford",
         placa: "hj768",
         color: "celeste"
     }
-
-
 ]
 
 
 const mantenimiento=[
     {
-        idmanteniento: 312,
-        idvehiculo: 154,
+        id: 1,
+        idvehiculo: 1,
         idconcepto: 123,
         fecha_mantenimiento:{dia:27, mes:09, año:2022},
         detalle:"se procede a quitar la bujia desgastada y reemplazarla por una nueva bujia ",
     },
-    
-    
-    
     {
-        idmanteniento: 254,
-        idvehiculo: 543,
+        id: 254,
+        idvehiculo: 223,
         idconcepto: 222,
         fecha_mantenimiento:{dia:02, mes:04, año:2020},
         detalle:"se procede a hacer debida revision y hacer el cambio de lallanta afectada",
     },
-    
     {
-        idmanteniento: 456,
-        idvehiculo: 334,
+        id: 456,
+        idvehiculo: 33,
         idconcepto: 363,
         fecha_mantenimiento:{dia:18, mes:07, año:2021},
         detalle:"se procede a buscar la fuga de aire acondcionado en cañerias , y se procede areemplazar por una nueva",
     },
     {
-        idmanteniento: 475,
-        idvehiculo: 443,
+        id: 475,
+        idvehiculo: 7,
         idconcepto: 411,
         fecha_mantenimiento:{dia:16, mes:01, año:2019},
         detalle:" se hace revision de aceite , y se reemplaza por un nuevo aceite de calidad",
     },
     {
-        idmanteniento: 565,
-        idvehiculo: 543,
+        id: 565,
+        idvehiculo: 5,
         idconcepto: 534,
         fecha_mantenimiento:{dia:18, mes:12, año:2022},
         detalle:"se procede a cambiar por una nueva bateria o a regargar la misma para evitar estos fallos",
     }
-    ]
+]
 
 //FUNCIONES CON PROMISES//
-function idmanteniento(id){
+function buscarmantenimiento(id){
     return new Promise((resolve, reject)=>{
-        const mantenimiento = mantenimiento.find((mantenimiento)=> mantenimiento.id===id );
-        if (!mantenimiento)
+        const mantenimientos = mantenimiento.find((mantenimientos)=> mantenimientos.id===id );
+        if (!mantenimientos)
         {
             const error= new Error();
-            error.message="el mantenimineto solicitado no se ha realizado";
+            error.message="El mantenimiento solicitado no se ha realizado";
             reject(error);
         }
-        resolve(mantenimiento);
+        resolve(mantenimientos);
     })
 }
 
-function autos(id){
+function autosfunction(id){
     return new Promise((resolve, reject)=>{
-        const autos =  autos.find((autos)=>{
-            return autos.id===id;
+        const auto =  autos.find((auto)=>{
+            return auto.id===id;
         });
-        if (!autos)
+        if (!auto)
         {
             const error =  new Error();
             error.message="No pudimos encontrar el mantenimiento del auto";
             reject(error);
         }
-        resolve(autos);
+        resolve(auto);
     })
 }
 
-mantenimiento(0)
-.then((mantenimiento)=>{ 
-    console.log(mantenimiento);
-    return mantenimiento(mantenimiento.idmanteniento);
+buscarmantenimiento(1)
+.then((mantenimientos)=>{ 
+    console.log(mantenimientos);
+    return autosfunction(mantenimientos.idvehiculo);
 })
-.then((mantenimiento)=>{
-    console.log(mantenimiento)
+.then((auto)=>{
+    console.log(auto)
 })
 .catch((error)=>{
     console.log(error.message)
-})   
-
+})
 
 //Funciones de Callbacks//
 
