540f213a487e7cf7ca3ed58d99f8f8a749512f49
EsleiterAvila <esleiterdidier-12@hotmail.com>
2022-10-21 20:19:55 -0500

    cambios de codigo edicion de html y js

--- parcial 1/Practica4-1p/index.js
+++ parcial 1/Practica4-1p/index.js
@@ -1,1 +1,52 @@
-//Protocolo HTTP y servidores Web, Arquitectura para API
+const path = require("path");
+
+const express = require("express");
+const cors =  require("cors");
+
+const PUERTO = 4040;
+//sdfsdf
+
+const urlautos = path.join(__dirname,"./Public/autos.html")
+const urlconcepto = path.join(__dirname,"./Public/concepto.html")
+const urlmantenimiento = path.join(__dirname,"./Public/mantenimiento.html")
+
+ 
+const server =  express();
+
+server.use(cors()).use(express.json())
+
+
+server.get('/', functionIndex )
+
+server.get('/concepto', (req,res)=>{
+    res.status(200).sendFile(urlconcepto);
+    // res.status(200).send({
+    //     response:"Acerca de!!!!!!"
+    // })
+})
+
+server.get('/autos', (req,res)=>{
+    res.status(200).sendFile(urlautos);
+    // res.status(200).send({
+    //     response:"Acerca de!!!!!!"
+    // })
+})
+
+server.get('/mantenimiento', (req,res)=>{
+    res.status(200).sendFile(urlmantenimiento);
+    // res.status(200).send({
+    //     response:"Acerca de!!!!!!"
+    // })
+})
+
+function functionIndex (req, res){
+    res.status(200).sendFile (urlautos);
+}
+
+
+server.listen(PUERTO, ()=>{
+    console.log(`Servidor corriendo http://localhost:${PUERTO}`);
+})
+
+
+
